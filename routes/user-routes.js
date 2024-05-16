583ea09ca7edd5fbba6464f3ed1fa4e725e08520
MapYh <145336421+MapYh@users.noreply.github.com>
2024-05-16 17:31:19 +0200

    Rnd

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,16 +4,28 @@ const { auth } = require("../middleware/auth");
 const { db } = require("../database/db.js");
 const router = Router();
 router.use(Router.json());
-/* router.post("/signup", signup);
-router.post("/login",  login); */
+router.post("/signup", signup);
+router.post("/login", login);
 
-//GET all users by username.
+//GET all users returns an array of objects with usernames.
 router.get("/users", (req, res) => {
   db.all("SELECT user_name FROM users", function (error, rows) {
     if (error) {
       res.status(400).json({ succcess: false, Message: error });
     } else {
       res.status(200).json({ succcess: true, Users: rows });
+    }
+  });
+});
+
+//GET all users with a specifick userID, returns an array of objects with usernames.
+router.get("/userID", (req, res) => {
+  const { user_id } = req.body;
+  db.all("SELECT user_name FROM users", function (error, rows) {
+    if (error) {
+      res.status(400).json({ succcess: false, Message: error });
+    } else {
+      res.status(200).json({ succcess: true, User: rows });
     }
   });
 });
