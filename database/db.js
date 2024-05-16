f68badb9d344053f19ce4f9b9ca80e4986a4909e
MapYh <145336421+MapYh@users.noreply.github.com>
2024-05-16 10:39:56 +0200

    Added a Get channels route to the index.js file

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ function initDatabase() {
     console.log("Connected to database");
   });
 
-  let sql_user = `CREATE TABLE IF NOT EXISTS users (
+  let sql_user = `CREATE TABLE IF NOT EXISTS "users" (
     user_id INTEGER PRIMARY KEY, 
     user_name VARCHAR(255),
     user_lastname VARCHAR(255), 
@@ -16,11 +16,20 @@ function initDatabase() {
     user_email TEXT UNIQUE
     )`;
 
-       db.serialize(() => {
-        db.run(sql_user, (error) => {
-          if (error) console.error("Error creating users table:", error.message);
-        });
+  let sql_channels = `CREATE TABLE IF NOT EXISTS "channels" (
+    channel_id INTEGER PRIMARY KEY, 
+    channel_name VARCHAR(255),
+    channel_owner_id INTEGER,
+    FOREIGN KEY(channel_owner_id) REFERENCES users(user_id)
+    )`;
+
+  db.serialize(() => {
+    db.run(sql_user, (error) => {
+      if (error) console.error("Error creating users table:", error.message);
+    }).run(sql_channels, (error) => {
+      if (error) console.error("Error creating users table:", error.message);
     });
+  });
   return db;
 }
 
--- index.js
+++ index.js
@@ -16,3 +16,15 @@ app.use("/api/channels", channelsRoutes);
 app.listen(PORT, URL, () => {
   console.log(`listenting to http://${URL}:${PORT}`);
 });
+
+/*-------GET----------- */
+
+app.get("/api/channels", (req, res) => {
+  db.get("SELECT channel_name FROM channels", function (error, rows) {
+    if (error) {
+      console.error(error);
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+});
