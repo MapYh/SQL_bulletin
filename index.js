77983478a6b667b9499505769a6dd598c925927a
MapYh <145336421+MapYh@users.noreply.github.com>
2024-05-16 12:29:34 +0200

    Added some basic code to create the GET route, amd a test rout for POST.

--- index.js
+++ index.js
@@ -20,11 +20,38 @@ app.listen(PORT, URL, () => {
 /*-------GET----------- */
 
 app.get("/api/channels", (req, res) => {
-  db.get("SELECT channel_name FROM channels", function (error, rows) {
+  db.all("SELECT channel_name FROM channels", function (error, rows) {
     if (error) {
       console.error(error);
     } else {
       res.status(200).json(rows);
     }
   });
+});
+
+app.get("/api/user", (req, res) => {
+  db.all("SELECT user_name FROM users", function (error, rows) {
+    if (error) {
+      console.error(error);
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+});
+
+/*-------POST----------- */
+//Exists to test the GET routes.
+app.post("/api/channels", (req, res) => {
+  const { channel_id, channel_name, channel_owner_id } = req.body;
+  db.run(
+    "INSERT INTO channels (channel_id, channel_name, channel_owner_id) VALUES (?, ?, ?) ",
+    [channel_id, channel_name, channel_owner_id],
+    function (error, rows) {
+      if (error) {
+        console.error(error);
+      } else {
+        res.status(200).json({ success: true, message: "Added a channel." });
+      }
+    }
+  );
 });
