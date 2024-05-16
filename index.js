be81d0aa930ed06759429d165e642291562896a3
Emadenni <emanyele.dng86@gmail.com>
2024-05-16 15:48:01 +0200

    adjusted code after merge

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const database = require("./database/db");
 const userRoutes = require("./routes/user-routes");
-const channelsRoutes = require("./routes/channels-routes");
-const tests = require("./tests/tests");
+const channelsRoutes = require("./routes/channels-routes")
+/* const tests = require("./tests/tests"); */
 const PORT = 8000;
 const URL = "127.0.0.1";
 
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
-/* 
-app.use("/api/channels", channelsRoutes);
-//För att testa GET routerna
-app.use("/api/test", tests);
-
 app.listen(PORT, URL, () => {
   console.log(`listenting to http://${URL}:${PORT}`);
-});
+}); 
+
+
+/* 
+app.use("/api/channels", channelsRoutes); */
+//För att testa GET routerna
+/* app.use("/api/test", tests); */
+
+
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,11 @@
 const Router = require("express");
 const { db } = require("../database/db.js");
+const { signup, login } = require("../controllers/account-controller.js");
 const router = Router();
 router.use(Router.json());
-/* router.post("/signup", signup);
-router.post("/login",  login); */
+
+router.post("/signup", signup);
+router.post("/login", login); 
 
 //GET all users by username.
 router.get("/users", (req, res) => {
