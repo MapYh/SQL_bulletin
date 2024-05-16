0e29fafee4a56ec7f062bbd67034d9468736996b
Emadenni <emanyele.dng86@gmail.com>
2024-05-16 11:02:29 +0200

    imported auth middleware in user 'user-routes', ready to test for authentication

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { signup, login } = require("../controllers/account-controller");
+const { auth } = require("../middleware/auth");
 
 const router = Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login",  login);
 
 module.exports = router;
